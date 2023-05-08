649de8d32de40369a0e02b94d9fe1f0f67ba7629
eladfain <62603008+eladfain@users.noreply.github.com>
2023-05-08 16:05:57 +0300

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -1,17 +1,5 @@
 let userScore = 0,
 computerScore = 0,
-<<<<<<< Updated upstream
-totalRound = 0;
-const userScore_span = document.getElementById("user_score"),
-computerScore_span = document.getElementById("computer_score"),
-scoreBoard_section = document.querySelector(".score_board"),
-result_p = document.querySelector(".result p"),
-rock_figure = document.getElementById("r"),
-paper_figure = document.getElementById("p"),
-scissors_figure = document.getElementById("s"),
-finalResult = document.querySelector("game-finish"),
-start_b = document.getElementById("start_button");
-=======
 totalRound = 0,
 gameOn=true;
 const ROCK='rock',
@@ -26,7 +14,7 @@ userScore_span = document.getElementById("user_score"),
  scissors_figure = document.getElementById(SCISSORS),
  start_b = document.getElementById("start_button");
  
->>>>>>> Stashed changes
+
 
 function computerOptions(){
     const computerSelection = Math.floor(Math.random() * 3);
@@ -42,21 +30,9 @@ function computerOptions(){
     };
 }
 
-<<<<<<< Updated upstream
-function convertToWord(letter){
-    if(letter === "r"){
-        return "Rock";
-    }else if(letter === "p"){
-        return "Paper";
-    }else
-        return "Scissors";
-}
 
-function win(userChoice, computerChoice){
-=======
 function playround(userChoice,computerChoice,outcome){
     let glowClass;
->>>>>>> Stashed changes
     const smallUserWord = "User:".fontsize(3).sup();
     const smallComputerWord = "Computer:".fontsize(3).sup();
     const userChoice_div = document.getElementById(userChoice);
@@ -108,10 +84,6 @@ function game(userChoice){
                     }
                 }    
             }
-<<<<<<< Updated upstream
-            totalRound++;
-=======
->>>>>>> Stashed changes
             if(userScore===5 || computerScore===5){
                 console.log(`you have played 5 rounds the score is User:${userScore}  Computer:${computerScore}`);
                 finishGame(); 
@@ -126,11 +98,7 @@ function finishGame(){
     const massegeSpan=document.createElement('span');
     massegeSpan.innerText=message;
     document.querySelector("#game-finish").appendChild(massegeSpan);
-<<<<<<< Updated upstream
-
-=======
     gameOn=false;
->>>>>>> Stashed changes
 }
 
 function startButton(){
@@ -139,63 +107,30 @@ function startButton(){
     userScore_span.innerText = 0;
     computerScore_span.innerText = 0;
     result_p.innerText = 'First to 5 points wins!';  
-<<<<<<< Updated upstream
-    console.clear();
-}
-
-function destroy() {
-    let olddata = document.getElementById("game-finish").lastChild;
-    document.getElementById("game-finish").removeChild(olddata);
-}
-=======
     document.querySelector('#game-finish').innerHTML='';
     console.clear();
     gameOn=true;
 }
 
 
->>>>>>> Stashed changes
 
-function stopFunction(e) {
-        if(userScore === 5 || computerScore === 5){
-            alert('The game has finished click okay to see your score!')
-                e.preventDefault();
-        } 
-}
 
 function playGame(){ 
     rock_figure.addEventListener('click', function() {
-<<<<<<< Updated upstream
-        game("r"); 
-        stopFunction(event); 
-        console.log("You clicked Rock"); 
-    })   
-    paper_figure.addEventListener('click', function() {
-        game("p");
-        stopFunction(event); 
-        console.log("You clicked Paper");
-=======
+
         game(ROCK);  
     }) 
 
     paper_figure.addEventListener('click', function() {
         game(PAPER);
->>>>>>> Stashed changes
     }) 
     
     scissors_figure.addEventListener('click', function() {
-<<<<<<< Updated upstream
-        game("s");
-        stopFunction(event); 
-        console.log("You clicked Scissors");
-=======
         game(SCISSORS);
->>>>>>> Stashed changes
     }) 
     
     start_b.addEventListener('click', function() {
         startButton();
-        destroy();
     })
     
 }
